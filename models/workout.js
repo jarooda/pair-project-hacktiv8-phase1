8913ca24215129fe91dfa60085b086ff986613e3
Rahutomos <rahutomos@gmail.com>
2020-11-20 01:55:35 +0700

    Rahutomos

--- models/workout.js
+++ models/workout.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Workout.belongsToMany(models.user, {through: 'UserWorkout'})
+      Workout.belongsToMany(models.User, {through: 'UserWorkout'})
     }
   };
   Workout.init({
